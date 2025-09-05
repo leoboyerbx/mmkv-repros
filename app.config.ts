e0fd7966d45090742fbf56ea76029ef739e6b51d
leoboyerbx <contact@leoboyer.dev>
2025-09-05 12:01:43 +0200

    base repro repo

--- app.config.ts
+++
@@ -1,8 +0,0 @@
-
-import { ConfigContext, ExpoConfig } from "expo/config";
-import 'tsx/cjs';
-
-module.exports = ({ config }: ConfigContext): Partial<ExpoConfig> => ({
-  ...config,
-  plugins: [...config.plugins!, ['./plugins/withMMKV.ts']],
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { createMMKV, useMMKVString } from 'react-native-mmkv';
 
-const storage = createMMKV()
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -16,7 +14,6 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
-  const [testValue, setTestValue] = useMMKVString('test');
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
